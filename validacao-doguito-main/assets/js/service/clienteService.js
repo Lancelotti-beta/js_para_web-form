e473f978b199d77724682637cd403a1b27a95f79
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-04 17:46:32 -0300

    Criando uma promise | Usando .then()
    ultilizado uma promise para fazer a requisição de um objeto, validar e
    adicionar a tabela de Clientes, utilizando o then

--- validacao-doguito-main/assets/js/service/clienteService.js
+++ validacao-doguito-main/assets/js/service/clienteService.js
@@ -1,9 +1,45 @@
-const http = new XMLHttpRequest()
+const adionaClienteNaLista = (nome, email) => {
 
-http.open('GET', 'http://localhost:3000/profile')
-http.send()
+    const elementoTr = document.createElement('tr')
+    const elementoCliente = `
+        <td class="td" data-td>${nome}</td>
+            <td>${email}</td>
+            <td>
+             <ul class="tabela__botoes-controle">
+                 <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                 <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>
+    `
+    elementoTr.innerHTML = elementoCliente
 
-http.onload = () => {
-    const data = http.response
-    console.log(data)
+    return elementoTr
 }
+
+const corpoDaTabela = document.querySelector('[data-tabela]')
+
+const listaDeClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest()
+        http.open('GET', 'http://localhost:3000/profile')
+
+        http.onload = () => {
+            if (http.status >= 400) {
+                reject(JSON.parse(http.response)) 
+            } else {
+                resolve(JSON.parse(http.response))
+            }
+        }
+        
+        http.send()
+    })
+    console.log(promise)
+    return promise
+}
+
+listaDeClientes()
+.then( resposta => {
+    resposta.forEach(elemento => {
+        corpoDaTabela.appendChild(adionaClienteNaLista(elemento.nome, elemento.email))
+    })
+})
