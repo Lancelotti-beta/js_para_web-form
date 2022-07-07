4b1d862291d6a32ac0303cbf3b7e458ab8bf866a
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-07 20:36:20 -0300

    Alterando a promise para fecth
    refatorando o código e trocando a 'Promise' por 'fetch' | facilitando a
    requisição e conversão no arquivo '.json'

--- validacao-doguito-main/assets/js/service/clienteService.js
+++ validacao-doguito-main/assets/js/service/clienteService.js
@@ -19,22 +19,23 @@ const adionaClienteNaLista = (nome, email) => {
 const corpoDaTabela = document.querySelector('[data-tabela]')
 
 const listaDeClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-        http.open('GET', 'http://localhost:3000/profile')
-
-        http.onload = () => {
-            if (http.status >= 400) {
-                reject(JSON.parse(http.response)) 
-            } else {
-                resolve(JSON.parse(http.response))
-            }
-        }
-        
-        http.send()
+    return fetch('http://localhost:3000/profile')
+    .then( resposta => {
+        return resposta.json()
     })
-    
-    return promise
+    // const promise = new Promise((resolve, reject) => {
+    //     const http = new XMLHttpRequest()
+    //     http.open('GET', 'http://localhost:3000/profile')
+    //     http.onload = () => {
+    //         if (http.status >= 400) {
+    //             reject(JSON.parse(http.response)) 
+    //         } else {
+    //             resolve(JSON.parse(http.response))
+    //         }
+    //     }
+    //     http.send()
+    // })
+    // return promise
 }
 
 listaDeClientes()
