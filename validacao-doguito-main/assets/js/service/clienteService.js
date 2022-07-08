6cbdbb3c55e78979198fed0de58372157b655bef
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-08 14:08:24 -0300

    Criando <scripe> para novo cliente
    Adicionando funcionalidade para atualizar a lista de clientes com novos
    lientes cadastrados | Criando funcionanlidade

--- validacao-doguito-main/assets/js/service/clienteService.js
+++ validacao-doguito-main/assets/js/service/clienteService.js
@@ -5,6 +5,23 @@ const listaDeClientes = () => {
     })
 }
 
+const criarCliente = (nome, email) => {
+    return fetch('http://localhost:3000/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/JSON'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        return resposta.body
+    })
+}
+
 export const serviceCliente = {
-    listaDeClientes
+    listaDeClientes,
+    criarCliente
 }
